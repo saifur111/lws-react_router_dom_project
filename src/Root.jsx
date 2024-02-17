a37dedc83b9bacba8f09fce14b3871d737544cbe
saifur111 <ssaaiiffuurr240@gmail.com>
2024-02-17 23:25:36 +0600

    solve some bug

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,62 +1,94 @@
-import { Form, Link, Outlet,useLoaderData } from "react-router-dom";
+import { useEffect } from "react";
+import { Outlet, useLoaderData, useNavigation } from "react-router";
+import { Form, NavLink, useSubmit } from "react-router-dom";
 
 export default function Root() {
-  const { contacts } =useLoaderData();
+    const { contacts, q } = useLoaderData();
+    const navigation = useNavigation();
+
+    const submit = useSubmit();
+
+    useEffect(() => {
+        document.getElementById("q").value = q;
+    }, [q]);
+
+    const searching =
+        navigation.location &&
+        new URLSearchParams(navigation.location.search).has("q");
+
     return (
-      <>
-        <div id="sidebar">
-          <h1>React Router Contacts</h1>
-          <div>
-            <form id="search-form" role="search">
-              <input
-                id="q"
-                aria-label="Search contacts"
-                placeholder="Search"
-                type="search"
-                name="q"
-              />
-              <div
-                id="search-spinner"
-                aria-hidden
-                hidden={true}
-              />
-              <div
-                className="sr-only"
-                aria-live="polite"
-              ></div>
-            </form>
-            <Form method="post">
-              <button type="submit">New</button>
-            </Form>
-          </div>
-          <nav>
-          {contacts.length ? (
-            <ul>
-              {contacts.map((contact) => (
-                <li key={contact.id}>
-                  <Link to={`contacts/${contact.id}`}>
-                    {contact.first || contact.last ? (
-                      <>
-                        {contact.first} {contact.last}
-                      </>
+        <>
+            <div id="sidebar">
+                <h1>React Router Contacts</h1>
+                <div>
+                    <Form id="search-form" role="search">
+                        <input
+                            id="q"
+                            aria-label="Search contacts"
+                            placeholder="Search"
+                            type="search"
+                            name="q"
+                            defaultValue={q}
+                            onChange={(e) => {
+                                const isFirstSearch = q == null;
+                                submit(e.currentTarget.form, {
+                                    replace: !isFirstSearch,
+                                });
+                            }}
+                            className={searching ? "loading" : ""}
+                        />
+                        <div
+                            id="search-spinner"
+                            aria-hidden
+                            hidden={!searching}
+                        />
+                        <div id="search-spinner" aria-hidden hidden={true} />
+                        <div className="sr-only" aria-live="polite"></div>
+                    </Form>
+                    <Form method="post">
+                        <button type="submit">New</button>
+                    </Form>
+                </div>
+                <nav>
+                    {contacts.length ? (
+                        <ul>
+                            {contacts.map((contact) => (
+                                <li key={contact.id}>
+                                    <NavLink
+                                        to={`contacts/${contact.id}`}
+                                        className={({ isActive, isPending }) =>
+                                            isActive
+                                                ? "active"
+                                                : isPending
+                                                ? "pending"
+                                                : ""
+                                        }
+                                    >
+                                        {contact.first || contact.last ? (
+                                            <>
+                                                {contact.first} {contact.last}
+                                            </>
+                                        ) : (
+                                            <i>No Name</i>
+                                        )}{" "}
+                                        {contact.favorite && <span>★</span>}
+                                    </NavLink>
+                                </li>
+                            ))}
+                        </ul>
                     ) : (
-                      <i>No Name</i>
-                    )}{" "}
-                    {contact.favorite && <span>★</span>}
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          ) : (
-            <p>
-              <i>No contacts</i>
-            </p>
-          )}
-        </nav>
-        </div>
-        <div id="detail">
-          <Outlet />
-        </div>
-      </>
+                        <p>
+                            <i>No contacts</i>
+                        </p>
+                    )}
+                </nav>
+            </div>
+            <div
+                id="detail"
+                className={navigation.state === "loading" ? "loading" : ""}
+            >
+                <Outlet />
+            </div>
+        </>
     );
-  }
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,34 +1,64 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom/client";
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import "./index.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Contact from "./Contact";
+import EditContact from "./EditContact";
+import ErrorPage from "./Error";
+import Index from "./Index";
 import Root from "./Root";
-import ErrorPage from "./ErrorPage";
-import Contact from "./contact";
-import { getContactsLoader } from "./loaders/contactsLoader";
-import { createContactAction } from "./actions/contactsActions";
+import {
+    createContactAction,
+    deleteContactAction,
+    editContactAction,
+    updateContactFavorite,
+} from "./actions/contactsActions";
+import "./index.css";
+import { getContactLoader, getContactsLoader } from "./loaders/contactsLoader";
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    errorElement: <ErrorPage />,
-    loader:getContactsLoader,
-    action:createContactAction,
-    children: [
-      {
-        path: "contacts/:contactId",
-        element: <Contact />,
-      },
-    ],
-  },
+    {
+        path: "/",
+        element: <Root />,
+        errorElement: <ErrorPage />,
+        loader: getContactsLoader,
+        action: createContactAction,
+        children: [
+            {
+                errorElement: <ErrorPage />,
+                children: [
+                    {
+                        index: true,
+                        element: <Index />,
+                    },
+                    {
+                        path: "/contacts/:contactId",
+                        element: <Contact />,
+                        loader: getContactLoader,
+                        action: updateContactFavorite,
+                    },
+                    {
+                        path: "/contacts/:contactId/edit",
+                        element: <EditContact />,
+                        loader: getContactLoader,
+                        action: editContactAction,
+                    },
+                    {
+                        path: "/contacts/:contactId/destroy",
+                        action: deleteContactAction,
+                        errorElement: (
+                            <div>
+                                Oops! There was an error deleting the contact!
+                            </div>
+                        ),
+                    },
+                ],
+            },
+        ],
+    },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
 );
